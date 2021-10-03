64fb328298b79398948f2dd10a7f43795562d03f
fredhgy <fredhgy2010@gmail.com>
2021-10-03 19:14:49 +0200

    data manage page

---
+++ update_locations.php
@@ -0,0 +1,31 @@
+<?php
+$dbhost = 'localhost';  // mysql服务器主机地址
+$dbuser = 'root';            // mysql用户名
+$dbpass = '19950611';          // mysql用户名密码
+$dbname = 'my_db';          // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// 设置编码，防止中文乱码
+mysqli_query($conn , "set names utf8");
+
+$id = $_GET['id'];
+$name = $_GET['n'];
+$lat = $_GET['lat'];
+$lng = $_GET['lng'];
+$type = $_GET['type'];
+$review = $_GET['r'];
+$review_details = $_GET['rd'];
+
+$sql = "UPDATE locations SET name='$name',lat='$lat',lng='$lng',type='$type',review='$review',review_details='$review_details' WHERE id='$id'";
+
+ 
+mysqli_select_db( $conn, $dbname );
+$retval = mysqli_query( $conn, $sql );
+if(! $retval )
+{
+  die('无法更新数据: ' . mysqli_error($conn));
+}else{
+    echo "数据更新成功\n";
+}
+mysqli_close($conn);
+?>
